b30a1fe3bda3712dd0045618c43be3d707819544
KanuRaj <rajputkaran410@gmail.com>
2020-08-16 12:08:14 +0530

    Updates

---
+++ precache-manifest.63d2dca108d6ae8bd7fc09cc84571c54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "527280517dc9955eccc4399ef9e73c44",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6fbd543647467119fc93",
+    "url": "./static/css/main.39602da5.chunk.css"
+  },
+  {
+    "revision": "e68104068a46f887d04a",
+    "url": "./static/js/2.6becbc3d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.6becbc3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fbd543647467119fc93",
+    "url": "./static/js/main.a5e3ac3b.chunk.js"
+  },
+  {
+    "revision": "64bc40e5b243fc8a4130",
+    "url": "./static/js/runtime-main.fa07d246.js"
+  }
+]);
